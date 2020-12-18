e715418c99b4cf6924b62d5f7946505bc9d378d9
zsuzitor <Stanislav_Avdosev@epam.com>
2020-12-18 18:07:07 +0400

    rep start

--- Working/Models/BL/Repositories/WorkerRepository.cs
+++ Working/Models/BL/Repositories/WorkerRepository.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using Working.Models.BL.Repositories.Interfaces;
+using Working.Models.BO.Domain;
 
 namespace Working.Models.BL.Repositories
 {
-    class WorkerRepository
+    public class WorkerRepository : IWorkerRepository
     {
+        public Task AddAsync(TaskToWork tsk)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<TaskToWork> GetNotRunnigTasks()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateAsync(TaskToWork tsk)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
